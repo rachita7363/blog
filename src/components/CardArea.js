17c4e7bfeb54c0a0b91c3ac9f6cb088bd8515a7f
rachita7363 <rachitav35@gmail.com>
2021-09-07 06:45:28 +0530

    adds download cv

--- src/components/CardArea.js
+++ src/components/CardArea.js
@@ -3,13 +3,15 @@ import '../styles/cardArea.css';
 import profilePic from '../assets/profile_pic.jpg';
 import Avatar from 'react-avatar';
 import Button from 'react-bootstrap/Button'
-import resume from '../assets/VenkateshTata_CV.pdf'
+import resume from '../assets/VenkateshTataCV.pdf'
 
 
 
 
 function CardArea() {
-
+    const downloadFile = () => {
+        window.location.href = "https://yoursite.com/src/assets/VenkateshTata_CV.pdf"
+      }
     return (
         <div className="root-wrapper">
             <div className="root-div">
@@ -26,7 +28,7 @@ function CardArea() {
                     <p className="intro-text">Hi! I am Venkatesh. I founded euclid.ai, a software development and hardware design company headquartered in Bangalore, India. My current primary interest is to build computer-vision based privacy preserving deep learning models that could be scaled into a decentralized ecosystem. Check out my startup at euclid.ai. My current role in the company is to design and build scalable cloud architecture for IoT systems integrating On-device learning for privacy preserving AI into an ecosystem binding edge devices and deep learning together for better understanding the user’s product usage patterns.</p>
                 </div>
                 <div className="cv-area">
-                <Button className="cv-btn" variant="outline-dark" ><a href = {resume} target = "_blank">Download CV</a></Button>
+                <a href = {resume} target = "_blank"><Button variant="outline-dark">Download CV</Button></a>
                 </div>
 
 
